729e6f786bdaa9e3526ee5e9775774f3c8fe0be6
filippovskiy09 <arturfilippovskij@gmail.com>
2024-04-09 10:40:19 +0300

    fixed errors

--- common/utils/generateRandomUser.util.ts
+++ common/utils/generateRandomUser.util.ts
@@ -2,7 +2,5 @@ import { v4 as uuid } from 'uuid';
 
 export const generateRandomUser = () => {
 	const names = ['Alice', 'Bob', 'Charlie', 'Diana', 'Edward', 'Fiona', 'George', 'Hannah', 'Ian', 'Julia'];
-
 	return {name: names[Math.floor(Math.random() * names.length)], id: uuid()}
-
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.scss";
 const inter = Inter({ subsets: ["latin"] });
+import '../styles/globals.scss';
 
 export const metadata: Metadata = {
   title: "Create Next App",
